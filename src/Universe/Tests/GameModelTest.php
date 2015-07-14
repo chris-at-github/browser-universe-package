a95da9bc8ef5ef169e5193a74808a3af587c6b19
Christian Pschorr <pschorr.christian@gmail.com>
2015-07-14 22:37:09 +0200

    Tuesday run

---
+++ src/Universe/Tests/GameModelTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Universe\Tests;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class GameModelTest extends TestCase {
+
+}
