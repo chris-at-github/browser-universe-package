34316f98c2acbe341e99cef1c92525ca444c080f
Christian Pschorr <pschorr.christian@gmail.com>
2015-07-22 06:09:20 +0200

    add mutator getMapAttribute

--- src/Universe/Models/Game.php
+++ src/Universe/Models/Game.php
@@ -12,4 +12,16 @@ class Game extends Universe {
 	 * @var string
 	 */
 	protected $table = 'games';
+
+	/**
+	 * returns the map object that assign to the game
+	 *
+	 * @param string HID from the map
+	 * @return \Universe\Models\Maps
+	 */
+	public function getMapAttribute($hid) {
+		return \App::make('\Universe\Repositories\MapsRepository')->findBy([
+			'hid'	=> $hid
+		]);
+	}
 }
