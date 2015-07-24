5f3fe57b470f7327f136d5cc035173afacfc6f38
Christian Pschorr <pschorr.christian@gmail.com>
2015-07-24 06:21:01 +0200

    Einbindung Browserify

--- src/Universe/Views/Footer.blade.php
+++ src/Universe/Views/Footer.blade.php
@@ -1,6 +1,6 @@
 {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
-<script src="{{URL::asset('js/jquery.js')}}"></script>
-<script src="{{URL::asset('js/underscore.js')}}"></script>
+<script src="{{URL::asset('js/vendor/jquery.js')}}"></script>
+<script src="{{URL::asset('js/vendor/underscore.js')}}"></script>
 <script src="{{URL::asset('js/universe.js')}}"></script>
 </body>
 </html>
