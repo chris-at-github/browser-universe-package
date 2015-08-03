31bcab636ca5ec6193163f76a3c2227227b2125f
Christian Pschorr <pschorr.christian@gmail.com>
2015-08-03 06:54:08 +0200

    implement axis option

--- src/Universe/Views/Playground/Dragable.blade.php
+++ src/Universe/Views/Playground/Dragable.blade.php
@@ -5,6 +5,8 @@
 <div id="drag-container">
 	<div class="dragable first"></div>
 </div>
-<div class="dragable"></div>
+{{--<div class="dragable"></div>--}}
+
+<div id="test-map"></div>
 
 @include('Universe::Footer')
