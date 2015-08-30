b88844b738d498ee8f754d4d715b677bf58217de
Christian Pschorr <pschorr.christian@gmail.com>
2015-08-30 15:34:37 +0200

    Stand heute

--- src/Universe/Controllers/PlaygroundController.php
+++ src/Universe/Controllers/PlaygroundController.php
@@ -4,4 +4,8 @@ class PlaygroundController extends UniverseController {
 	public function dragable() {
 		return \View::make('Universe::Playground.Dragable');
 	}
+
+	public function snap() {
+		return \View::make('Universe::Playground.Snap');
+	}
 }
---
+++ src/Universe/Views/Playground/Snap.blade.php
@@ -0,0 +1,9 @@
+@include('Universe::Header')
+
+<header>Playground/Snap</header>
+
+<svg id="snap">
+
+</svg>
+
+@include('Universe::Footer')
